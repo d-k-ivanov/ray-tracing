528653be226b5dbe831b5a4e107cd68fa78945e0
Dmitry Ivanov <d.k.ivanov@live.com>
2024-01-25 00:56:28 +0100

    implemented the first step of RayTracingInOneWeekend using RGBA image

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -38,7 +38,7 @@ public:
         // m_ViewportHeight = static_cast<uint32_t>(ImGui::GetContentRegionAvail().y);
 
         constexpr double aspectRatio = 16.0 / 9.0;
-        m_ViewportWidth  = static_cast<uint32_t>(400);
+        m_ViewportWidth              = static_cast<uint32_t>(400);
         // m_ViewportWidth  = static_cast<uint32_t>(ImGui::GetContentRegionAvail().x);
         m_ViewportHeight = static_cast<uint32_t>(m_ViewportWidth / aspectRatio);
         m_ViewportHeight = (m_ViewportHeight < 1) ? 1 : m_ViewportHeight;
@@ -47,8 +47,7 @@ public:
         {
             const ImVec2 center = ImVec2(
                 (ImGui::GetWindowSize().x - static_cast<float>(m_Image->GetWidth())) * 0.5f,
-                (ImGui::GetWindowSize().y - static_cast<float>(m_Image->GetHeight())) * 0.5f
-            );
+                (ImGui::GetWindowSize().y - static_cast<float>(m_Image->GetHeight())) * 0.5f);
             ImGui::SetCursorPos(center);
             ImGui::Image(
                 m_Image->GetDescriptorSet(),
@@ -72,11 +71,29 @@ public:
             m_ImageData = new uint32_t[static_cast<uint64_t>(m_ViewportWidth * m_ViewportHeight)];
         }
 
-        for(uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+        for(uint32_t j = 0; j < m_ViewportHeight; j++)
         {
-            m_ImageData[i] = Random::UInt();
-            m_ImageData[i] |= 0xff000000;
+            for(uint32_t i = 0; i < m_ViewportWidth; i++)
+            {
+                const auto r = static_cast<double>(i) / (m_ViewportWidth - 1);
+                const auto g = static_cast<double>(j) / (m_ViewportHeight - 1);
+                const auto b = 0;
+                const auto a = 255;
+
+                const auto ir = static_cast<uint8_t>(255.999 * r);
+                const auto ig = static_cast<uint8_t>(255.999 * g);
+                const auto ib = static_cast<uint8_t>(255.999 * b);
+                const auto ia = static_cast<uint8_t>(255.999 * a);
+
+                m_ImageData[j * m_Image->GetWidth() + i] = (ia << 24) | (ib << 16) | (ig << 8) | ir;
+            }
         }
+
+        // for(uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+        // {
+        //     m_ImageData[i] = Random::UInt();
+        //     m_ImageData[i] |= 0xff000000;
+        // }
 
         m_Image->SetData(m_ImageData);
 
